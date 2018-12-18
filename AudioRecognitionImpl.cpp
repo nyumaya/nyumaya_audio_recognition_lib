8721c001ef2177e1096057d9cd5564a0bd5555ff
Günther Neumair <g.neumair@tuta.io>
2018-12-18 23:27:59 +0100

    Update Smoothing Parameters

--- AudioRecognitionImpl.cpp
+++ AudioRecognitionImpl.cpp
@@ -220,7 +220,7 @@ int AudioRecognitionImpl::smooth_detection(uint8_t*scores,int size)
 
 	//exclude index 0 which is _unknown_
 	for(int i = 1 ; i < size; ++i){
-		if(scores[i] > 200 + 55*sensitivity ){
+		if(scores[i] >=230 + 25*sensitivity ){
 			last_frames[i]->push_front(scores[i]);
 		} else {
 			last_frames[i]->push_front(0);
@@ -244,7 +244,7 @@ int AudioRecognitionImpl::smooth_detection(uint8_t*scores,int size)
 	}
 	
 
-	if(biggest_score >  (200 + 455*sensitivity) && cooldown == 0){
+	if(biggest_score >  (1275*sensitivity) && cooldown == 0){
 		cooldown = detection_cooldown;
 		last_frames[biggest_score_key]->clear();
 		return biggest_score_key;
