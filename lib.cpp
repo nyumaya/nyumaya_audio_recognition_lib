e9defdecb6da19500890ab3c916118b01306b935
Günther Neumair <g.neumair@yahoo.com>
2019-10-05 14:38:59 +0200

    Change API to unified naming style Add JNI interface

--- lib.cpp
+++ lib.cpp
@@ -4,70 +4,177 @@
 
 #include "lib.h"
 
+#include "libjni.h"
+
+const char*VersionString = "0.3.5";
 
 
-SpeakerVerificationImpl* create_speaker_verification(const char*modelPath)
+jbyteArray ToJavaByteArray(JNIEnv* env,const uint8_t* bytes,size_t len) 
 {
-	std::string str(modelPath);
-	return new SpeakerVerificationImpl(str);
+	jbyteArray byte_array = env->NewByteArray(len);
+	env->SetByteArrayRegion(
+		byte_array, 0, len, reinterpret_cast<const jbyte*>(bytes));
+	return byte_array;
 }
 
-uint8_t* VerifySpeaker(SpeakerVerificationImpl*impl,const uint8_t* const data,const int mel_length)
+	
+jstring Java_com_nyumaya_audiorecognition_NyumayaLibrary_getVersionString(JNIEnv *env, jobject obj)
 {
-	return impl->VerifySpeaker(data, mel_length);
+	return env->NewStringUTF( VersionString);
+}
+
+jlong Java_com_nyumaya_audiorecognition_NyumayaLibrary_createFeatureExtractor(JNIEnv *env, jobject obj,jint nfft,jint melcount, jint sample_rate,
+	jint lowerf, jint upperf, jfloat window_len,jfloat shift){
+	return (jlong) new FeatureExtractor(nfft,melcount,sample_rate,lowerf,upperf,window_len,shift);
+}
+
+jbyteArray Java_com_nyumaya_audiorecognition_NyumayaLibrary_signalToMel(JNIEnv *env, jobject obj,jlong impl,jbyteArray pcm,jfloat gain)
+{
+	FeatureExtractor* f = (FeatureExtractor*) impl;
+	int inlen = env->GetArrayLength(pcm);
+	jbyte *pcm_arr 	= env->GetByteArrayElements(pcm, 0);
+
+	//FIXME: Properly get required size
+	uint8_t result[1024];
+	int outlen = f->signal_to_mel((int16_t *)pcm_arr,inlen,result,gain);
+
+	env->ReleaseByteArrayElements(pcm, pcm_arr, 0);
+
+	return ToJavaByteArray(env,result,outlen);
+}
+
+jint Java_com_nyumaya_audiorecognition_NyumayaLibrary_getMelcount(JNIEnv *env, jobject obj,jlong impl)
+{
+	FeatureExtractor* f = (FeatureExtractor*) impl;
+	return f->get_melcount();
 }
 
 
-AudioRecognitionImpl* create_audio_recognition(const char* modelPath)
+void Java_com_nyumaya_audiorecognition_NyumayaLibrary_deleteFeatureExtractor(JNIEnv *env, jobject obj,jlong impl)
 {
-	std::string str(modelPath);
-	return new AudioRecognitionImpl(str);
+	FeatureExtractor* f = (FeatureExtractor*) impl;
+	delete f;
+}
+
+void Java_com_nyumaya_audiorecognition_NyumayaLibrary_deleteAudioRecognition(JNIEnv *env, jobject obj,jlong impl)
+{
+	AudioRecognitionImpl* f = (AudioRecognitionImpl*) impl;
+	delete f;
 }
 
 
-int RunDetection(AudioRecognitionImpl*impl,const uint8_t* const data,const int mel_length)
+
+jlong Java_com_nyumaya_audiorecognition_NyumayaLibrary_createAudioRecognition(JNIEnv *env, jobject obj)
+{
+	return (jlong) new AudioRecognitionImpl();
+}
+
+
+
+void Java_com_nyumaya_audiorecognition_NyumayaLibrary_setSensitivity(JNIEnv *env, jobject obj,jlong impl,jfloat sensitivity)
+{
+
+	AudioRecognitionImpl* f = (AudioRecognitionImpl*) impl;
+	f->SetSensitivity(sensitivity);
+}
+
+jint Java_com_nyumaya_audiorecognition_NyumayaLibrary_runDetection(JNIEnv *env, jobject obj,jlong impl,jbyteArray mels)
+{
+	AudioRecognitionImpl* f = (AudioRecognitionImpl*) impl;
+
+	int inlen = env->GetArrayLength(mels);
+	jbyte *mels_arr = env->GetByteArrayElements(mels, 0);
+
+	return f->RunDetection((uint8_t*)mels_arr,inlen);
+}
+
+jint Java_com_nyumaya_audiorecognition_NyumayaLibrary_loadModelFromBuffer(JNIEnv *env, jobject obj,jlong impl,jbyteArray modelData){
+	AudioRecognitionImpl* f = (AudioRecognitionImpl*) impl;
+
+	int len = env->GetArrayLength(modelData);
+	jbyte *binaryModel = env->GetByteArrayElements(modelData, 0);
+
+	return f->LoadModelFromBuffer((const char*)binaryModel,len);
+}
+
+
+
+AudioRecognitionImpl* createAudioRecognition()
+{
+	return new AudioRecognitionImpl();
+}
+
+void deleteAudioRecognition(AudioRecognitionImpl*impl)
+{
+	delete impl;
+	impl = 0;
+}
+
+const char* getVersionString()
+{
+	return VersionString;
+}
+
+
+int runDetection(AudioRecognitionImpl*impl,const uint8_t* const data,const int mel_length)
 {
 	return impl->RunDetection(data,mel_length);
 }
 
-uint8_t*RunRawDetection(AudioRecognitionImpl*impl,const uint8_t* const data,const int mel_length)
+uint8_t*runRawDetection(AudioRecognitionImpl*impl,const uint8_t* const data,const int mel_length)
 {
 	return impl->RunRawDetection(data,mel_length);
 }
 
-void SetSensitivity(AudioRecognitionImpl*impl,float sens)
+void setSensitivity(AudioRecognitionImpl*impl,float sens)
 {
 	 impl->SetSensitivity(sens);
 }
 
 
-size_t GetInputDataSize(AudioRecognitionImpl*impl)
+
+int openModel(AudioRecognitionImpl*impl,const std::string& modelPath)
+{
+	impl->OpenModel(modelPath);
+}
+
+
+size_t getInputDataSize(AudioRecognitionImpl*impl)
 {
 	return impl->get_input_data_size();
 }
-	
 
-FeatureExtractor* create_feature_extractor(size_t nfft,size_t melcount,size_t sample_rate,
+
+int loadModelFromBuffer(AudioRecognitionImpl*impl,const char*binaryModel,int len)
+{
+	return impl->LoadModelFromBuffer(binaryModel,len);
+}
+
+FeatureExtractor* createFeatureExtractor(size_t nfft,size_t melcount,size_t sample_rate,
                     size_t lowerf, size_t upperf,float window_len,float shift)
 {
 	return new FeatureExtractor(nfft,melcount,sample_rate,lowerf,upperf,window_len,shift);
 }
+	
+void deleteFeatureExtractor(FeatureExtractor*impl)
+{
+	delete impl;
+	impl = 0;
+}
 
-int signal_to_mel(FeatureExtractor*impl,const int16_t * const pcm, size_t len,uint8_t*result,float gain){
+
+int signalToMel(FeatureExtractor*impl,const int16_t * const pcm, size_t len,uint8_t*result,float gain)
+{
 	return impl->signal_to_mel(pcm,len,result,gain);
 }
 
 
-size_t get_melcount(FeatureExtractor*impl){
+size_t getMelcount(FeatureExtractor*impl)
+{
 	return impl->get_melcount();
-
 }
 
 
-void print_mel_filter(FeatureExtractor*impl){
-	return impl->print_mel_filter();
-
-}
 
 
 
---
+++ libjni.h
@@ -0,0 +1,54 @@
+#ifndef NYUMAYA_AUDIO_RECOGNITION_JNI_H
+#define NYUMAYA_AUDIO_RECOGNITION_JNI_H
+
+#include <string>
+#include <jni.h>
+
+class AudioRecognitionImpl;
+class FeatureExtractor;
+
+
+#define DEFAULT_VIS  __attribute__ ((visibility ("default") ))
+
+extern "C"
+{
+	JNIEXPORT jstring Java_com_nyumaya_audiorecognition_NyumayaLibrary_getVersionString(JNIEnv *env, jobject obj);
+
+	JNIEXPORT jlong Java_com_nyumaya_audiorecognition_NyumayaLibrary_createFeatureExtractor(JNIEnv *env, jobject obj,
+		jint nfft,jint melcount, jint sample_rate,jint lowerf, jint upperf, jfloat window_len,jfloat shift);
+
+
+	JNIEXPORT void Java_com_nyumaya_audiorecognition_NyumayaLibrary_deleteFeatureExtractor(JNIEnv *env, jobject obj,jlong impl);
+
+	JNIEXPORT void Java_com_nyumaya_audiorecognition_NyumayaLibrary_deleteAudioRecognition(JNIEnv *env, jobject obj,jlong impl);
+
+
+	JNIEXPORT jbyteArray Java_com_nyumaya_audiorecognition_NyumayaLibrary_signalToMel(JNIEnv *env, jobject obj,jlong impl,jbyteArray pcm,jfloat gain);
+
+	JNIEXPORT jint Java_com_nyumaya_audiorecognition_NyumayaLibrary_getMelcount(JNIEnv *env, jobject obj,jlong impl);
+
+	JNIEXPORT jlong Java_com_nyumaya_audiorecognition_NyumayaLibrary_createAudioRecognition(JNIEnv *env, jobject obj);
+
+	JNIEXPORT jint Java_com_nyumaya_audiorecognition_NyumayaLibrary_loadModelFromBuffer(JNIEnv *env, jobject obj,jlong impl,jbyteArray modelData);
+
+	JNIEXPORT void Java_com_nyumaya_audiorecognition_NyumayaLibrary_setSensitivity(JNIEnv *env, jobject obj,jlong impl,jfloat sensitivity);
+
+	JNIEXPORT jint Java_com_nyumaya_audiorecognition_NyumayaLibrary_runDetection(JNIEnv *env, jobject obj,jlong impl,jbyteArray mels);
+
+
+/*	//Audio Recognition
+	
+	//Input Mel Features and get the raw probabilities of the labels
+	uint8_t*RunRawDetection(AudioRecognitionImpl*impl,const uint8_t* const data,const int mel_length) DEFAULT_VIS;
+
+	size_t GetInputDataSize(AudioRecognitionImpl*impl) DEFAULT_VIS;
+*/
+
+
+}
+
+
+
+
+
+#endif
